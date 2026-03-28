88c0cd8190008f182dc7f38a3dbea5a568073684
anchikov_aa <anchikov_aa@da.gov.ru>
2026-03-29 02:00:11 +0300

    первый залив

---
+++ src/src/src/test/java/FirstTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class FirstTest {
+    @Test
+    public void googleSearchTest() {
+        // Настройки
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+
+        // Открываем Google
+        open("https://www.google.com");
+
+        // Проверяем заголовок
+        String title = title();
+        System.out.println("Page title: " + title);
+
+        // Ищем текст на странице
+        $("body").shouldHave(text("Google"));
+
+        // Закрываем браузер
+        closeWebDriver();
+    }
+}
+
